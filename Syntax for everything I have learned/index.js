3f3d4da305efb94566b5c72214c40a239a6c9373
BremnerCoding <BremnerCoding@gmail.com>
2023-06-13 17:51:51 +0100

    Syntax for JavaScript Fundamentals

---
+++ Syntax for everything I have learned/index.js
@@ -0,0 +1,42 @@
+//How to Declare Primitive Data Types
+const number = 4;
+const string = "string";
+const boolean = true;
+const variable = null;
+const undefined = undefined;
+//Haven't learnt symbol or bigint data types yet
+
+//How to Declare an Array
+const array = [1, "two", 3, "four", 5, true, null];
+
+//How to Declare an Object
+const object = {
+    name: "Andrew Bremner",
+    age: 24,
+    likeDogs: true,
+    hasGirlfriend: null,
+};
+
+//Syntax for a for loop
+for(let i = 0; i < array.length; i++) {
+    //Code block to execute
+};
+
+//Syntax for if/else if/else statements
+if(number = 3) {
+    //Code block to execute
+} else if (number = 5) {
+    //Code block to execute
+} else {
+    //Code block to execute
+};
+
+//Syntax for a for in loop
+for(const key in object) {
+    //Code block to execute
+}
+
+//Syntax for a function
+function nameOfFunction() {
+    //Code block to execute
+}
